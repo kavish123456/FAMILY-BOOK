97708ed5eb57c3c58028c4d447d2fb7830ae6ff7
kavish123456 <87981736+kavish123456@users.noreply.github.com>
2021-10-19 12:41:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","Kavish pic.jpg", "sonal pic.jpg", "Varun pic.jpg"];
+var names = ["Fmaily Book","Kavish Agarwal", "Sonal Agarwal", "Varun Kumar Agarwal"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 3;
+    if(i > numbers_of_family_member_in_array)
+    {i = 0;}
+    var updatedImage = [images[i]];
+    var updatedName = [names[i]] ;
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
